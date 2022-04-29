df270d777e90a9b36554f5acc1759767c4a13e9c
Camille <folcamille@yahoo.fr>
2022-04-29 11:06:00 +0200

    fetch front

--- polyfront/src/App.js
+++ polyfront/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+
+  function envoyer() {
+    fetch('http://localhost:4000',{
+      method: "POST",
+      body: JSON.stringify({text})
+    })
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={event => setText(event.target.value)}/>
+      <input type="Button" onClick={envoyer} value="envoyer"/>
     </div>
   );
 }
